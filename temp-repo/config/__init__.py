baf0d8f44c2c003d67919a2dc49466308414b85b
xxfer <2565474095@qq.com>
2024-04-20 23:01:03 +0800

    🎉 init: 初始化项目

--- temp-repo/config/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base_config import *
-from .db_config import *
--- temp-repo/proxy/providers/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author  : relakkes@gmail.com
-# @Time    : 2024/4/5 10:13
-# @Desc    :
-from .jishu_http_proxy import new_jisu_http_proxy
-from .kuaidl_proxy import new_kuai_daili_proxy
